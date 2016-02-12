152d7c8b116b120162b0bdcc3ae06c74b3ecc29d
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2016-02-12 19:12:38 +0000

    Add watch/build tasks to watch src directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,10 @@
 var gulp = require('gulp')
-var babelify = require('babelify')
-var watchify = require('watchify')
-var liveReactLoad = require('livereactload')
-var browserify = require('browserify')
-var uglify = require('gulp-uglify')
-var source = require('vinyl-source-stream')
-var buffer = require('vinyl-buffer')
-var sourcemaps = require('gulp-sourcemaps')
-var gutil = require('gulp-util')
-var gulpif = require('gulp-if')
-var assign = require('lodash').assign
-
-var customOpts = {
-  entries: 'js/index.jsx',
-  extensions: ['.jsx']
-}
-
-var watchOpts = assign({}, watchify.args, customOpts)
-var opts = assign({}, customOpts)
-
-gulp.task('babel', babel)
-gulp.task('watch', watch)
+var shell = require('gulp-shell')
 
 function watch () {
-  var b = watchify(browserify(watchOpts))
-  b.plugin(liveReactLoad, {})
-  b.transform(babelify.configure({stage: 0}))
-  b.on('update', bundle.bind(null, b))
-  b.on('log', gutil.log)
-  return bundle(b)
+  console.log('running watch')
+  gulp.watch('src/**/*.js*', ['build'])
 }
 
-function babel () {
-  var b = browserify(opts)
-  b.transform(babelify.configure({stage: 0}))
-  return bundle(b)
-}
-
-function bundle (b) {
-  return b.bundle()
-  .on('error', gutil.log)
-  .pipe(source('bundle.js'))
-  .pipe(buffer())
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(gulpif(process.env.MODE === 'prod', uglify()))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('dist'))
-}
+gulp.task('watch', watch)
+gulp.task('build', shell.task(['npm run build-examples']))
