f3d4fd7c07206fa80bc53498caf6d0cae7972ff2
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2016-03-07 20:05:24 +0000

    self-host roboto font

--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,7 @@ var config = require('./config.js')
 var app = express()
 
 app.use(express.static('dist'))
+app.use(express.static('fonts'))
 app.use(express.static('node_modules'))
 app.use('config.js', express.static('config.js'))
 app.use('lib', express.static('lib'))
