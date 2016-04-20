3a52347422ea488535c4da58fe2bc00bf4108681
jpkeyw <jpolirer@keywcorp.com>
2016-04-20 16:22:07 +0000

    Closes #30 Closes #32 upgrade react-leaflet

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,19 +1,13 @@
 import React, {Component, PropTypes} from 'react'
 import {
-  /* LayersControl, */
+  LayersControl,
+  LayerGroup,
   Map as LeafletMap,
-  TileLayer,
-  ZoomControl
+  Marker,
+  Popup,
+  TileLayer
 } from 'react-leaflet'
-import {LayerGroup} from './LayerGroup'
-
-const baseLayer = (
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    layerName='OpenStreetMap'
-    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-  />
-)
+const {BaseLayer, Overlay} = LayersControl
 
 export class Map extends Component {
   static propTypes = {
@@ -27,31 +21,53 @@ export class Map extends Component {
     zoomControlPosition: 'topleft'
   }
 
+  createMarkersList () {
+    return this.props.markers.map((marker) => (
+      <Marker
+        key={marker.key}
+        position={marker.position}
+      >
+        <Popup>
+         <span>{marker.children}</span>
+        </Popup>
+      </Marker>
+    ))
+  }
+
   render () {
-    const {center, /* title, */zoomControlPosition} = this.props
-
-    const layerGroup = <LayerGroup {...this.props}/>
-
-    /* const base = {
-      'OpenStreetMap': baseLayer
-    } */
-
-    /* const overlay = {
-      [title]: layerGroup
-    } */
-
+    const {center, title} = this.props
     return (
       <LeafletMap
         center={center}
         minZoom={3}
         zoom={5}
-        zoomControl={false}
+        zoomControl={true}
       >
-        {baseLayer}
-        {layerGroup}
-        {/* <LayersControl baseLayers={base} overlays={overlay}>
-        </LayersControl>*/}
-        <ZoomControl position={zoomControlPosition} />
+        <LayersControl position='topright'>
+          <BaseLayer
+            checked
+            name='Color'
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            />
+          </BaseLayer>
+          <BaseLayer name='Black & White'>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+            />
+          </BaseLayer>
+          <Overlay
+            checked
+            name={title}
+          >
+            <LayerGroup>
+              {this.createMarkersList()}
+            </LayerGroup>
+          </Overlay>
+        </LayersControl>
       </LeafletMap>
     )
   }
