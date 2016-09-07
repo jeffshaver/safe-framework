814867fe4f4db61a323c8b03d5840650cad92ac0
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2016-09-07 12:56:07 +0000

    closes #89. add compression to example server

--- examples/server.js
+++ examples/server.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var config = require('./config.js')
+var compression = require('compression')
 var app = express()
 
+app.use(compression())
 app.use(express.static('dist'))
 app.use(express.static('fonts'))
 app.use(express.static('node_modules'))
